554f63aa6895ffb9b1a8132e1b9d024a99523110
Shao <yanjin@ShaodeMacBook-Air.local>
2019-01-03 19:50:31 +0800

    修改tabBar

--- src/assets/images/tabbar/index.js
+++ src/assets/images/tabbar/index.js
@@ -1,10 +1,10 @@
 const TabBar = {
     home:           require('./home_line_tab_icon.png'),
     home_active:    require('./home_tab_icon.png'),
-    list:           require('./list_icon.png'),
-    list_active:    require('./list_line_icon.png'),
-    my:             require('./my_icon.png'),
-    my_active:      require('./my_line_icon.png')
+    list:           require('./list_line_icon.png'),
+    list_active:    require('./list_icon.png'),
+    my:             require('./my_line_icon.png'),
+    my_active:      require('./my_icon.png'),
 };
 
 export default TabBar;
